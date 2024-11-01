4c1da683ea2e05178013423f44d8912f3404c2ed
Elsiant <31175927+Elsiant@users.noreply.github.com>
2024-11-01 21:15:34 +0900

    MapDrawingPanel 파일 추가

---
+++ MapEditor/MapEditor/Forms/Panels/MapDrawingPanel.cs
@@ -0,0 +1,49 @@
+﻿using MapEditor.Types;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapEditor.Forms.Panels
+{
+    class MapDrawingPanel : DrawingPanel
+    {
+        private int _column = 64;
+        private int _row = 64;
+        private int _currentLayer = 0;
+
+        private Tile[,] _tiles;
+        public MapDrawingPanel()
+        {
+            _tiles = new Tile[_column, _row];
+        }
+
+        public void SetImage(Point point, string fileName)
+        {
+            _tiles[point.X, point.Y] = new Tile(point);
+            //_tiles[point.X, point.Y] = new Tile(_fileName);
+        }
+
+        //public void MapDrawingPanel_Paint(object sender, PaintEventArgs e)
+        //{
+
+        //}
+
+        private void SaveData(string fileName)
+        {
+
+        }
+
+        private void LoadData(string fileName)
+        {
+
+        }
+
+        private void ChangeLayer(int layer)
+        {
+            _currentLayer = layer;
+        }
+    }
+}

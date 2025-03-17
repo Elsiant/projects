d32c46640dcd16ff6f7e99a42b5651e4eb8b0c09
YeoHyeonGu <>
2025-03-17 15:47:18 +0900

    RelayCommand<T>추가로 제네릭 타입 지원,

--- MyWidgets/MyWidgets/Commands/RelayCommand.cs
+++ MyWidgets/MyWidgets/Commands/RelayCommand.cs
@@ -7,6 +7,46 @@ using System.Windows.Input;
 
 namespace MyWidgets.Commands
 {
+    class RelayCommand<T> : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        private readonly Action<T> _execute;
+        private readonly Func<T, bool>? _canExecute;
+
+        public RelayCommand(Action<T> execute)
+            : this(execute, null)
+        {
+
+        }
+
+        public RelayCommand(Action<T> execute, Func<T, bool>? canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute == null)
+            {
+                return true;
+            }
+
+            return _canExecute((T)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
     class RelayCommand : ICommand
     {
         public event EventHandler? CanExecuteChanged;
